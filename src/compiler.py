6d8597e097515641b19bc9a2c303a3805354ec14
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2020-02-02 14:56:01 -0800

    better commenting to compiler.py

--- src/compiler.py
+++ src/compiler.py
@@ -5,13 +5,18 @@ from parser import Parser
 
 def main():
     code = open(sys.argv[1]).read()
+
+    # Tokenize
     T = Tokenizer()
     tokens = T.tokenize(code)
     for t in tokens:
         print(t)
 
+    # Parse
     P = Parser()
     tree = P.parse(tokens)
+
+    # Validate
     tree.validate()
     print(tree.to_str())
 
